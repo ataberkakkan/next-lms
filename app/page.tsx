a7b48f6981810ce8eac7e66bf2aaad11f6b1ae93
ataberkakkan <akkanataberk@gmail.com>
2025-06-20 16:35:08 +0300

    Implement Better-Auth & Github SignIn

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,36 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { authClient } from "@/lib/auth-client";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
+
 export default function Home() {
-  return <div></div>;
+  const router = useRouter();
+
+  const { data: session } = authClient.useSession();
+
+  const signOut = async () => {
+    await authClient.signOut({
+      fetchOptions: {
+        onSuccess: () => {
+          router.push("/login");
+          toast.success("Signed Out Successfully");
+        },
+      },
+    });
+  };
+
+  return (
+    <div>
+      {session ? (
+        <div>
+          <p>{session.user.name}</p>
+          <Button onClick={signOut}>Log Out</Button>
+        </div>
+      ) : (
+        <Button onClick={() => router.push("/login")}>Log In</Button>
+      )}
+    </div>
+  );
 }
