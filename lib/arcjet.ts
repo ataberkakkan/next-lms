75dfad22869e04d903c7872e01864d7e69841a2c
ataberkakkan <akkanataberk@gmail.com>
2025-06-23 11:35:27 +0300

    Implement Arcjet

---
+++ lib/arcjet.ts
@@ -0,0 +1,27 @@
+import arcjet, {
+  detectBot,
+  fixedWindow,
+  protectSignup,
+  sensitiveInfo,
+  shield,
+  slidingWindow,
+} from "@arcjet/next";
+
+export {
+  detectBot,
+  fixedWindow,
+  protectSignup,
+  sensitiveInfo,
+  shield,
+  slidingWindow,
+};
+
+export default arcjet({
+  key: process.env.ARCJET_KEY!,
+  characteristics: ["fingerprint"],
+  rules: [
+    shield({
+      mode: "LIVE",
+    }),
+  ],
+});
